aa32ab06cbcaeca90dfaa4ee91ce37b7e93653f4
刘思远 <liusiyua23@mails.tsinghua.edu.cn>
2024-12-08 11:48:10 +0800

    fix a bug

--- code/sparkium/app/built_in_scenes.cpp
+++ code/sparkium/app/built_in_scenes.cpp
@@ -41,7 +41,7 @@ void LoadSunFlowerDog(Scene *scene)
       asset_manager->LoadMesh(Mesh(vertices, indices), "LightMesh");
   Material light_material;
   light_material.base_color = {0.0f, 0.0f, 0.0f};
-  light_material.emission = {1.0f, 1.0f, 0.6f};
+  light_material.emission = {1.0f, 1.0f, 1.0f};
   light_material.emission_strength = 30.0f;
   int light_id = scene->CreateEntity();
   scene->SetEntityMesh(light_id, light_mesh_id);
